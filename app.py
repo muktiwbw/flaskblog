25ca506e86da3f86fb7998a76034f7a2e40c4ead
Mukti Wibowo <muktiw7@gmail.com>
2020-06-01 01:37:32 +0700

    - Added separate files for views. - Use fake data to temporarily simulate data - Learn to use templating engine for FOR loop and IF

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Import Flask module
-from flask import Flask
+from flask import Flask, render_template
 
 # Initialize flask app
 app = Flask(__name__)
@@ -8,11 +8,37 @@ app = Flask(__name__)
 # Start with the route name, and then the handler below it
 @app.route('/')
 def home():
-    return 'Home Page'
+    # Data simulation. Wouldn't be used in the future
+    posts = [
+        {
+            'title': 'Blog Post One',
+            'content': 'This is the first blog post.',
+            'author': 'Abra',
+            'date_posted': '12 May 2020'
+        },
+        {
+            'title': 'Blog Post Two - Electric Boogaloo',
+            'content': 'This is the second blog post.',
+            'author': 'Cadabra',
+            'date_posted': '13 May 2020'
+        }
+    ]
+
+    # It's nice to encapsulate all the data into one dictionary
+    data = {
+        'posts': posts
+    }
+
+    return render_template('home.html', data=data)
 
 @app.route('/about')
 def about():
-    return 'About Page'
+    title = 'About'
+
+    data = {
+        'title': title
+    }
+    return render_template('about.html', data=data)
 
 # This one's for running flask via python filename.py
 if __name__ == '__main__':
