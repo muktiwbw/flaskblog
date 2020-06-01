6053270e7a9179af541a005b4e4b1df6b728c45a
Mukti Wibowo <muktiw7@gmail.com>
2020-06-02 03:40:50 +0700

    Added forms - Created form classes (registration and login) - Added validators - Added allowed methods in routes

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-# Import Flask module
+# Import modules
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 # Initialize flask app
 app = Flask(__name__)
+
+# Secret key for my app
+app.config['SECRET_KEY'] = '862948fbdeb3cd375249b67465d67658'
 
 # Routes
 # Start with the route name, and then the handler below it
@@ -38,7 +42,28 @@ def about():
     data = {
         'title': title
     }
+
     return render_template('about.html', data=data)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+
+    data = {
+        'form': form
+    }
+
+    return render_template('forms/registration.html', data=data)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+
+    data = {
+        'form': form
+    }
+
+    return render_template('forms/login.html', data=data)
 
 # This one's for running flask via python filename.py
 if __name__ == '__main__':
