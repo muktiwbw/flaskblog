86c7a43151657f756891c03d472528645c3a0199
Mukti Wibowo <muktiw7@gmail.com>
2020-06-03 15:14:51 +0700

    Added custom validations on User model: - Unique username validation - Unique email validation

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -28,6 +29,23 @@ class RegistrationForm(FlaskForm):
                             ])
 
     submit = SubmitField('Sign Up')
+
+    # Custom validations
+    # def validate_fieldname(self, fieldname):
+    #     if True:
+    #         raise ValidationError('messages')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+
+        if user:
+            raise ValidationError('Username has already taken.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+
+        if user:
+            raise ValidationError('Email has already taken.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
