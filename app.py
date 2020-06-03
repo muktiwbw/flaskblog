93dfdba75cb26fcf5cdede71bb9ae2bd5f465c6f
Mukti Wibowo <muktiw7@gmail.com>
2020-06-03 08:15:26 +0700

    Database added - Uses SQLite - Created User and Post model - Uses REST convention for table names (plural)

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # Import modules
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 # Initialize flask app
@@ -7,10 +9,40 @@ app = Flask(__name__)
 
 # Secret key for my app
 app.config['SECRET_KEY'] = '862948fbdeb3cd375249b67465d67658'
+# Database query string
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+# Initialize database model
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', {self.date_posted})"
+
 
 # Routes
 # Start with the route name, and then the handler below it
 @app.route('/')
+@app.route('/home')
 def home():
     # Data simulation. Wouldn't be used in the future
     posts = [
